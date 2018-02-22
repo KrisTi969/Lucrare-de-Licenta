eb01f77dae805d3094fc606cbe0fd8274834acc2
KrisTi969 <crys_gold_ten@yahoo.ca>
2018-02-22 16:21:15 +0200

    fixed tvProduct page not showing last div when clicking

--- app/view/tvProduct.php
+++ app/view/tvProduct.php
@@ -136,7 +136,7 @@
                                 <span class="pull-right"><i class="fa fa-minus"></i></span>
                                 <div class="clearfix"></div>
                             </a>
-                            <div id="#productCollapse" class="collapse collapseDiv">
+                            <div id="productCollapse" class="collapse collapseDiv">
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             </div>
                         </div> <!-- End single product extra div -->
